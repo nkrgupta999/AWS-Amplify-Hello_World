dafa110d0f029983b80a41ffa421b4e6cb205051
nkrgupta999 <nitinkumargupta999@outlook.com>
2024-07-07 21:59:10 +0530

    Add file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World from Caze Labs with AWS Amplify</h1>
+    <h1>Hello World from Caze Labs with Nitin AWS Amplify</h1>
   );
 }
 
